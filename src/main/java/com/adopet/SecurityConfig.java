306a6eb6d412c2f612ee34b5ffe9e84fa3452e11
Wesley M. <55067868+WesleyMime@users.noreply.github.com>
2024-11-29 02:34:06 -0300

    feat: cors configuration for front end

---
+++ src/main/java/com/adopet/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.adopet;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain config(HttpSecurity http) throws Exception {
+        http
+                .cors((cors) -> {
+                    cors.configurationSource(apiConfigurationSource());
+                })
+                .authorizeHttpRequests((requests) -> {
+                    requests.anyRequest().permitAll();
+                })
+                .csrf(AbstractHttpConfigurer::disable)
+        ;
+        return http.build();
+    }
+
+    UrlBasedCorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
